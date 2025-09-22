acc9669536c4c6ee51fbee6e928896b54157204e
my-2002 <2321973256@qq.com>
2025-09-22 17:52:28 +0800

    add lm-evaluation-harness & modify template for deepseek-qwen-1.5b

--- datamix/config/eval_template/model_template.py
+++ datamix/config/eval_template/model_template.py
@@ -3,9 +3,9 @@ from opencompass.models import HuggingFaceBaseModel
 models = [
     dict(
         type=HuggingFaceBaseModel,
-        abbr='qwen2.5-7b-hf',
+        abbr='deepseek-r1-distill-qwen-1.5b-hf',
         path="{path_to_eval_model}",
-        max_out_len=2048,
+        max_out_len=32768,
         batch_size=16,
         run_cfg=dict(num_gpus=4),
     )
