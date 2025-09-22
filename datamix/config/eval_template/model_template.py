7c1f2771b83ba1f4c2f4ece138af6af68f7d4ba1
my-2002 <2321973256@qq.com>
2025-09-22 19:59:02 +0800

    remove lm-evaluation-harness temporary

--- datamix/config/eval_template/model_template.py
+++ datamix/config/eval_template/model_template.py
@@ -6,7 +6,7 @@ models = [
         abbr='deepseek-r1-distill-qwen-1.5b-hf',
         path="{path_to_eval_model}",
         max_out_len=32768,
-        batch_size=16,
+        batch_size=8,
         run_cfg=dict(num_gpus=4),
     )
 ]
